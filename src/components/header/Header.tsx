34c23bf057a9c1f488682ad050290b25d9473af7
MikhailTo <toshkin.mikhail@yandex.ru>
2024-10-22 22:46:15 +0300

    "Added Menu component to Header component"

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Logo from './Logo';
+import Menu from './Menu';
 
 const Header: React.FC = () => {
   return (
     <header>
       <Logo />
+      <Menu />
     </header>
   );
 };
