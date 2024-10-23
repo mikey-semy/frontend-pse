aeeb6a5097b4eaf476f19cfec0cce89275223a51
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-10-23 05:25:18 +0300

    Update searchQuestions.ts

--- src/api/search/searchQuestions.ts
+++ src/api/search/searchQuestions.ts
@@ -3,6 +3,6 @@ import { handleApiResponse, handleApiError } from '../utils';
 import { QuestionItem } from '../../types/';
 
 export const searchQuestions = (questionText: string): Promise<QuestionItem[]> =>
-    api.get<QuestionItem[]>(`/search`, { params: { q: questionText } })
+    api.get<QuestionItem[]>(`https://api.pse.aedb.online/search`, { params: { q: questionText } })
       .then(handleApiResponse)
       .catch(handleApiError);
