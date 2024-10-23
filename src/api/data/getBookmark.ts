5aa3aadf98369b27b493f572097ee38754db35fb
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-10-23 05:24:31 +0300

    Update getBookmark.ts

--- src/api/data/getBookmark.ts
+++ src/api/data/getBookmark.ts
@@ -2,6 +2,6 @@ import api from '../config';
 import { handleApiResponse, handleApiError } from '../utils';
 
 export const getBookmark = (): Promise<string> =>
-  api.get<string>('/static/scripts/bookmark.new.js')
+  api.get<string>('https://api.pse.aedb.online/static/scripts/bookmark.new.js')
     .then(handleApiResponse)
     .catch(handleApiError);
