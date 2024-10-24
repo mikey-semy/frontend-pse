ab2f5cef672f11f74049d295e9bd2ace96a2540d
Mikhail Toshkin <toshkin.mikhail@yandex.ru>
2024-10-24 05:12:22 +0000

    fix: background-color of nav and add menu item

--- src/data/menu/menuItems.ts
+++ src/data/menu/menuItems.ts
@@ -2,4 +2,5 @@ import { MenuItem } from '../../types';
 
 export const menuItems: MenuItem[] = [
     { id: 1, icon: '⚡️', title: 'Помощник', url: '/auto_search' },
+    { id: 2, icon: '⚙️', title: 'AEDB', url: 'https://aedb.online' },
 ];
