5430538ab9567ed91259a717a5bda3b98e371b43
Mikhail Toshkin <toshkin.mikhail@yandex.ru>
2024-10-24 12:30:14 +0000

    create toggle menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Logo from './Logo';
-import NavIcon from './NavIcon';
 import Menu from './Menu';
 
 const Header: React.FC = () => {
   return (
     <header>
       <Logo />
-      <NavIcon />
       <Menu />
     </header>
   );
--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -5,20 +5,39 @@ import { Link } from 'react-router-dom';
 const Menu: React.FC = () => {
 
     const [items] = useState(menuItems);
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen((prev) => !prev);
+    };
+
+    const handleLinkClick = () => {
+        setMobileMenuOpen(false);
+    };
+    
     return (
-        <nav className='nav'>
-            <ul className='nav__items'>
-                {items.map((item) => (
-                    <li className='nav__item' key={item.id}>
-                        <Link className='nav__item--link' to={item.url}>
-                            <span className='nav__item--icon'>{item.icon}</span>
-                            <span className='nav__item--title'>{item.title}</span>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <>
+            <button 
+                className="nav-toggle" 
+                onClick={toggleMobileMenu}> ☰ 
+            </button>
+            <nav className={`nav ${isMobileMenuOpen ? 'nav--open' : ''}`}>
+                <ul className='nav__items'>
+                    {items.map((item) => (
+                        <li className='nav__item' key={item.id}>
+                            <Link 
+                                className='nav__item--link' 
+                                to={item.url} 
+                                onClick={handleLinkClick}
+                            >
+                                <span className='nav__item--icon'>{item.icon}</span>
+                                <span className='nav__item--title'>{item.title}</span>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </>
     );
 };
 
