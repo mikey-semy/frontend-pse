75bf26146f41af6f310c6674c2deae9370fbd899
Mikhail Toshkin <toshkin.mikhail@yandex.ru>
2024-10-24 11:31:01 +0000

    add menu item

--- src/data/menu/menuItems.ts
+++ src/data/menu/menuItems.ts
@@ -1,6 +1,7 @@
 import { MenuItem } from '../../types';
 
 export const menuItems: MenuItem[] = [
-    { id: 1, icon: '⚡️', title: 'Помощник', url: '/auto_search' },
-    { id: 2, icon: '⚙️', title: 'AEDB', url: 'https://aedb.online' },
+    { id: 1, icon: '🔍', title: 'Поиск', url: '/' },
+    { id: 2, icon: '⚡️', title: 'Помощник', url: '/auto_search' },
+    { id: 3, icon: '⚙️', title: 'AEDB', url: 'https://aedb.online' },
 ];
